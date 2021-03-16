ec3903fa30d9af5190b61b7c2289ad77b88e6da8
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-03-16 22:26:55 +0100

    Audio hotovo, chybí úkol 2.

--- main.js
+++ main.js
@@ -9,19 +9,26 @@ let odstavec = document.querySelector('.odstavec');
 function odstavecOnMouseOver() {
     odstavec.classList.add('odstavec-bold');
 }
- function odstavecOnMouseOut() {
-     odstavec.classList.remove('odstavec-bold');
- }
+function odstavecOnMouseOut() {
+    odstavec.classList.remove('odstavec-bold');
+}
 
- function toggleRed() {
-     odstavec.classList.toggle('odstavec-red');
- }
+function toggleRed() {
+    odstavec.classList.toggle('odstavec-red');
+}
 
+console.log(odstavec);
  
- function biggerOnclick(udalost) {
-     //udalost.target.
-     console.log(udalost);
- };
+function increaseFontSize1px(udalost) {
+    //udalost.target.
+    console.log(udalost);
+    
+    //najdi font size
+    //převeď na int
+    //k font size přičti 1
+    currentFontSize++;
+};
+
 
 
  //<button onclick="zvetsitPismo(‚.odstavec, 1);">větší po kliknutí</button>
@@ -30,17 +37,39 @@ function odstavecOnMouseOver() {
  // https://www.w3schools.com/jsref/obj_event.asp - události
 
 
- // audio přehraj
+// audio přehraj
 function prehraj (elementSelector) {
     let audioElement = document.querySelector(elementSelector);
     audioElement.play();
 }
 
- // audio zastav
- function zastav (elementSelector) {
+// audio zastav
+function zastav (elementSelector) {
     let audioElement = document.querySelector(elementSelector);
     audioElement.pause();
- }
+}
+ 
+//audio ztiš úplně
+function ztisUplne (elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0;
+}
 
+//audio volume 50%
+function volumeHalf (elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0.5;
+}
 
- //buttony: přehraj, pauzni, ztišit, hlasitost tak akorát, hlasitost na max, nastav na začátek
+//audio volume max
+function volumeMax (elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 1;
+}
+
+//audio vrať na začátek
+function backToStart (elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.load();
+}
+
